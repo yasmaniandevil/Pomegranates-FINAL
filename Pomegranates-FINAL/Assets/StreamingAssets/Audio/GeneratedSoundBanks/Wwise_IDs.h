bcbd743c8290030e60a84516b744d02ad549b72b
yasmaniandevil <yasmine.batniji@gmail.com>
2024-04-03 08:33:08 -0400

    fixed wwise again

--- Pomegranates-FINAL/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Pomegranates-FINAL/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,47 +13,19 @@ namespace AK
 {
     namespace EVENTS
     {
-        static const AkUniqueID PLAY_AMB_ADANANDPEOPLE = 3184083507U;
-        static const AkUniqueID PLAY_AMB_CHILDREN = 3524268276U;
-        static const AkUniqueID PLAY_BIRDS = 3025897706U;
-        static const AkUniqueID PLAY_ENV_PORTALLEAD_01 = 3518020456U;
-        static const AkUniqueID PORTALENTER = 193618431U;
-        static const AkUniqueID PORTALEXIT = 1119349543U;
+        static const AkUniqueID PLAY_OCEAN = 1724638476U;
     } // namespace EVENTS
-
-    namespace GAME_PARAMETERS
-    {
-        static const AkUniqueID REVERSE = 3260481321U;
-    } // namespace GAME_PARAMETERS
-
-    namespace TRIGGERS
-    {
-        static const AkUniqueID PORTALENTERTRIGGER = 1920258857U;
-    } // namespace TRIGGERS
 
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
-        static const AkUniqueID AMB_ADANANDPEOPLE = 780465942U;
-        static const AkUniqueID AMB_PORTALB_CHILDREN = 3854491928U;
-        static const AkUniqueID ENV_PORTALLEAD = 534607965U;
-        static const AkUniqueID PALI_BIRDS_INTER = 3636604895U;
-        static const AkUniqueID PORTALEXIT = 1119349543U;
-        static const AkUniqueID PORTALOPEN = 2461641043U;
+        static const AkUniqueID OCEAN = 3802555985U;
     } // namespace BANKS
 
     namespace BUSSES
     {
-        static const AkUniqueID AMBIENT = 77978275U;
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
     } // namespace BUSSES
-
-    namespace AUX_BUSSES
-    {
-        static const AkUniqueID HIGH_PASS_FILTER = 3024472480U;
-        static const AkUniqueID LOW_PASS_FILTER = 3452161668U;
-        static const AkUniqueID REVERB = 348963605U;
-    } // namespace AUX_BUSSES
 
     namespace AUDIO_DEVICES
     {
